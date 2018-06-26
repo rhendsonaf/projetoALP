c9f4556fedb975a6337625d7371455ce2bf1ae34
Rhendson Alexandre <39617716+rhendsonaf@users.noreply.github.com>
2018-06-26 19:48:51 -0300

    Update funcoes.h

--- funcoes.h
+++ funcoes.h
@@ -9,8 +9,19 @@ typedef struct matriz{
     int columns;
 } matriz;
 
-void rand_Matriz(float mat[][LENGHT_MAX],struct matriz m){
-    //
+// This function allow the user to fill a matrix manually.
+void matrixFill(float mat[][LENGHT_MAX], struct matriz m){
+    for(int i = 0; i < m.rows; i++){
+        for(int j = 0; j < m.columns; j++){
+            printf("\nInsira o valor da linha %d e coluna %d: \n", i+1, j+1);
+            scanf("%f\n", &mat[i][j]);
+        }
+    }
+    printf("A Matriz foi preenchida com sucesso.\n");
+}
+
+// This function fill the matrix with random numbers. 
+void matrixRand(float mat[][LENGHT_MAX],struct matriz m){
     srand(time(NULL));
 
     for(int i = 0; i < m.rows; i++){
@@ -18,49 +29,45 @@ void rand_Matriz(float mat[][LENGHT_MAX],struct matriz m){
             mat[i][j] = rand() % 10;
         }
     }
-    printf("Matriz preenchida aleatoriamente.\n\n");
+    printf("A Matriz foi preenchida com valores aleatórios.\n");
 }
 
-void manu_Matriz(float mat[][LENGHT_MAX], struct matriz m){
+// This function print a matrix in the screen.
+void matrixPrint(float matriz[][LENGHT_MAX], struct matriz m){
+    printf("\n");
     for(int i = 0; i < m.rows; i++){
         for(int j = 0; j < m.columns; j++){
-            printf("\nDigite o valor da linha %d e coluna %d: \n", i+1, j+1);
-            scanf("%f", &mat[i][j]);
-        }
-    }
-    printf("Matriz preenchida manualmente.\n\n");
-}
-
-void imprime_Matriz(float matriz[][LENGHT_MAX], struct matriz m){
-    printf("-----------IMPRIMINDO MATRIZ-----------\n");
-    for(int i = 0; i < m.rows; i++){
-        for(int j = 0; j < m.columns; j++){
-            printf("%.2f ", matriz[i][j]);
+            printf("%.f ", matriz[i][j]);
         }
         printf("\n");
     }
     printf("\n");
 }
 
-matriz somaMatriz(float matA[][LENGHT_MAX], float matB[][LENGHT_MAX], float matC[][LENGHT_MAX], struct matriz mA){
+// This function make a sum of matrices (Arrays).
+matriz matrixSum(float matA[][LENGHT_MAX], float matB[][LENGHT_MAX], float matC[][LENGHT_MAX], struct matriz mA){
     for(int i = 0; i < mA.rows; i++){
         for(int j = 0; j < mA.columns; j++){
             matC[i][j] = matA[i][j] + matB[i][j];
         }
     }
-    printf("Soma executada, selecione a opção 12 pra visualizar a nova matriz C.\n\n");
+    printf("A soma de matrizes foi efetuada.\n\n");
     return mA;
 }
-matriz subMatriz(float matA[][LENGHT_MAX], float matB[][LENGHT_MAX], float matC[][LENGHT_MAX], struct matriz mA){
+
+// This function make a subtraction of matrices (Arrays).
+matriz matrixSub(float matA[][LENGHT_MAX], float matB[][LENGHT_MAX], float matC[][LENGHT_MAX], struct matriz mA){
     for(int i = 0; i < mA.rows; i++){
         for(int j = 0; j < mA.columns; j++){
             matC[i][j] = matA[i][j] - matB[i][j];
         }
     }
-    printf("Subtração executada, selecione a opção 12 pra visualizar a nova matriz C.\n\n");
+    printf("A subtração de matrizes foi efetuada.\n\n");
     return mA;
 }
-matriz multMatriz(float matA[][LENGHT_MAX], float matB[][LENGHT_MAX], float matC[][LENGHT_MAX], struct matriz mA, struct matriz mB){
+
+// This function make a multiplication of matrices (Arrays).
+matriz matrixMult(float matA[][LENGHT_MAX], float matB[][LENGHT_MAX], float matC[][LENGHT_MAX], struct matriz mA, struct matriz mB){
     struct matriz mC;
     mC.columns = mA.columns;
     mC.rows = mB.rows;
@@ -76,25 +83,26 @@ matriz multMatriz(float matA[][LENGHT_MAX], float matB[][LENGHT_MAX], float matC
             aux = 0;
         }
     }
-    printf("Multiplicação executada, pressione 12 para visualizar a nova matriz.\n");
+    printf("A multiplicação de matrizes foi efetuada.\n");
     return mC;
 }
 
-void setaMatArquivo(float mat[][LENGHT_MAX], struct matriz m){
+// This function read a matrix from a txt file.
+void readMatrix(float mat[][LENGHT_MAX], struct matriz m){
     FILE *f;
-    int l, c;
+    int r, c;
 
     f = fopen("home/niohggir/ProjectALP/matriz.txt","r");
 
     if (f == NULL) {
-        printf("O arquivo não foi localizado.");
+        printf("O arquivo não existe.");
         return;
     }
 
-    fscanf(f, "%d", &l);
+    fscanf(f, "%d", &r);
     fscanf(f, "%d", &c);
 
-    m.rows = l;
+    m.rows = r;
     m.columns = c;
 
     for(int i = 0; i < m.rows; i++){
@@ -103,20 +111,22 @@ void setaMatArquivo(float mat[][LENGHT_MAX], struct matriz m){
         }
     }
 
-    printf("A matriz foi preenchida pressione 10(se for A) ou 11(se for B) para visualiza-la");
+    printf("A matriz foi preenchida a partir do arquivo em questão.");
     fclose(f);
 }
-void escreveMatC(float matC[][LENGHT_MAX], struct matriz mC){
+
+// This function write a matrix in a txt file.
+void writeMatrix(float matC[][LENGHT_MAX], struct matriz mC){
     FILE *f;
-    f = fopen("home/niohggir/ProjectALP/matrizC.txt","w");
+    f = fopen("home/niohggir/ProjectALP/matrizC.txt", "w");
 
     fprintf(f, "%d %d \n", mC.rows, mC.columns);
     for(int i = 0; i < mC.rows; i++){
         for(int j = 0; j < mC.columns; j++){
-            fprintf(f,"%.2f ", matC[i][j]);
+            fprintf(f, "%.2f ", matC[i][j]);
         }
-        fprintf(f, "\n");
+        fprintf(f ,"\n");
     }
-    printf("Matriz estrita no arquivo, procure o arquivo para visualiza-la.");
+    printf("A matriz C foi escrita em um arquivo.");
     fclose(f);
 }
